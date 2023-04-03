decede42516338a07406521e12b06216f6b80966
Neil Sambhu <neilsambhu@gmail.com>
2023-04-03 11:36:27 -0400

    find ambulance

--- PythonAPI/examples/texture_sandbox/ambulance_texture.py
+++ PythonAPI/examples/texture_sandbox/ambulance_texture.py
@@ -17,14 +17,33 @@ except IndexError:
 # -- imports -------------------------------------------------------------------
 # ==============================================================================
 import carla
+import subprocess, time
+
+def kill_carla_podman():
+    kill_process = subprocess.Popen('podman kill --signal KILL -a', shell=True)
+    kill_process.wait()
+    time.sleep(1)
+    kill_process = subprocess.Popen('podman container cleanup --all --rm', shell=True)
+    kill_process.wait()
+    time.sleep(1)
+kill_carla_podman()
+
+# 4/1/2023 7:57:17 PM: launch simulator: start
+cmd = f'xhost local:root && podman run --privileged --net=host -e DISPLAY=$DISPLAY carlasim/carla:0.9.13 /bin/bash ./CarlaUE4.sh'
+server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+time.sleep(10)
+# 4/1/2023 7:57:17 PM: launch simulator: end
 
 # Connect to the CARLA simulator
 client = carla.Client('localhost', 2000)
 client.set_timeout(10.0)
 
 # Get the ambulance blueprint
-blueprint_library = client.get_blueprint_library()
-vehicle_bp = blueprint_library.find('vehicle.ambulance')
+blueprint_library = client.get_world().get_blueprint_library()
+#for item in blueprint_library:
+#	print(f'{item.id}')
+#vehicle_bp = blueprint_library.find('vehicle.ambulance')
+vehicle_bp = blueprint_library.find('vehicle.ford.ambulance')
 
 # Create a new material with the desired texture
 texture_file = 'colorful_cat.jpeg'
