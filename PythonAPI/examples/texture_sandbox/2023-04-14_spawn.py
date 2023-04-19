34553a91c2890d48285e75bb3b9e96f09392284f
neilsambhu <neilsambhu@gmail.com>
2023-04-18 20:44:12 -0400

    spawn only

--- PythonAPI/examples/texture_sandbox/2023-04-14_spawn.py
+++ PythonAPI/examples/texture_sandbox/2023-04-14_spawn.py
@@ -6,7 +6,7 @@ import glob
 import os
 import sys
 try:
-    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
@@ -26,6 +26,7 @@ def kill_carla_podman():
     kill_process = subprocess.Popen('podman container cleanup --all --rm', shell=True)
     kill_process.wait()
     time.sleep(1)
+'''
 kill_carla_podman()
 
 # 4/1/2023 7:57:17 PM: launch simulator: start
@@ -33,16 +34,16 @@ cmd = f'xhost local:root && podman run --privileged --net=host -e DISPLAY=$DISPL
 #server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
 time.sleep(10)
 # 4/1/2023 7:57:17 PM: launch simulator: end
-
+'''
 # Connect to the CARLA simulator
 client = carla.Client('localhost', 2000)
 client.set_timeout(10.0)
 
 # Get the world object
-# world = client.get_world()
+world = client.get_world()
 # Load layered map for Town 01 with minimum layout plus buildings and parked vehicles
 # world = client.load_world('Town01_Opt', carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
-world = client.load_world('Town04_Opt', carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
+# world = client.load_world('Town04_Opt', carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
 # world = client.load_world('Town10HD_Opt', carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
 # Toggle all buildings off
 # world.unload_map_layer(carla.MapLayer.Buildings)
@@ -68,7 +69,7 @@ blueprint_library = world.get_blueprint_library()
 vehicle_bp = blueprint_library.find('vehicle.ford.ambulance')
 
 # Spawn the vehicle
-for i, spawn_point in enumerate(spawn_points[0:100]):
+for i, spawn_point in enumerate(spawn_points[0:1]):
     vehicle = world.spawn_actor(vehicle_bp, spawn_point)
 
     # Wait for a few seconds to let the vehicle spawn properly
@@ -78,10 +79,11 @@ for i, spawn_point in enumerate(spawn_points[0:100]):
     print(f'Spawned vehicle {vehicle.id}')
 
 all_objects = list(world.get_names_of_all_objects())
-print(all_objects)
+#print(all_objects)
 ambulances = [k for k in all_objects if 'BP_Ambulance' in k]
-parked_vehicles = [k for k in all_objects if 'Vh_Car_' in k]
-# print(filter_objects)
+# parked_vehicles = [k for k in all_objects if 'Vh_Car_' in k]
+print(ambulances)
+'''
 # Load image texture
 from PIL import Image
 # Load the modified texture
@@ -116,5 +118,5 @@ from tqdm import tqdm
 #     new_color = carla.Color(255,0,0)
 #     # vehicle.set_color(new_color)
 #     vehicle.set_attribute('color', new_color.rgba)
-
+'''
 print('done')
